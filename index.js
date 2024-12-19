6be8cac133d50fb8a0d3fc0036b8251648bc38ce
Oketch <oketchngwani@gmail.com>
2024-12-19 20:17:20 +0900

    javascript functions

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// code your solution here
+ function saturdayFun(activity = "roller-skate") {
+  return `This Saturday, I want to ${activity}!`;
+}
+
+// 2. mondayWork function
+function mondayWork(activity = "go to the office") {
+  return `This Monday, I will ${activity}.`;
+}
+
+// 3. wrapAdjective function
+function wrapAdjective(flair = "*") {
+  return function(adjective = "special") {
+    return `You are ${flair}${adjective}${flair}!`;
+  };
+}
+
+// Example usage
+console.log(saturdayFun());                      
+console.log(saturdayFun("bathe my dog"));         
+
+console.log(mondayWork());                        
+console.log(mondayWork("work from home"));      
+
+const encouragingPromptFunction = wrapAdjective("!!!");
+console.log(encouragingPromptFunction("a dedicated programmer"));  
+
+console.log(wrapAdjective("%")("a dedicated programmer")); 
